5d5534378c631af152c37e3243e25f9225a49aff
Timo Eismar <eismar@gmail.com>
2024-03-06 11:22:16 +0100

    update

---
+++ bd.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title bd
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ./images/obsidian.png
+# @raycast.argument1 { "type": "text", "placeholder": "quick task", "optional": false, "percentEncoded": false}
+# @raycast.packageName Obsidian
+
+# Documentation:
+# @raycast.description Append to configured note
+# @raycast.author Timo E
+# @raycast.authorURL https://github.com/faxe360/raycast-obsidian-task-braindump
+
+# The name of your vault, e.g., "Knowledge Base"
+VAULT_PATH="YOUR PATH"
+VAULT_NAME="YOUR VAULT"
+TASK_FILE="YOUR FILE"
+
+if [ -z "$VAULT_NAME" ]
+then
+  echo "Configure VAULT_NAME"
+  exit 1
+fi
+
+cat << EOF >> "$VAULT_PATH/$VAULT_NAME/$TASK_FILE"
+- [ ] ${1}
+EOF
